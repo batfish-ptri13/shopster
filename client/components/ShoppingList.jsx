af129a0441a2cf7d2841527b6fe3a39cfe8a0a69
rmarkowi1990 <rick@fastcasual.co>
2024-01-11 18:14:54 -0800

    fixed merge conflicts

--- client/components/ShoppingList.jsx
+++ client/components/ShoppingList.jsx
@@ -31,7 +31,7 @@ export default function ShoppingList() {
 
   return (
     <>
-      <Nav/>
+      <Nav />
       < div className='shoppingListContainer' >
 
 
@@ -40,26 +40,8 @@ export default function ShoppingList() {
 
         <div id='shoppingGrid'>
 
-<<<<<<< HEAD
-        <div id='shoppingLeft'>
-          <h3>Store Inventory:</h3>
-          <ProductEntry item='Rice' />
-          <ProductEntry item='Chicken' />
-          <ProductEntry item='Tomato' />
-          <ProductEntry item='Red Pepper' />
-          <ProductEntry item='Green Pepper' />
-          <ProductEntry item='Onion' />
-          <ProductEntry item='Ketchup' />
-          <ProductEntry item='Mustard' />
-          <ProductEntry item='Tomato Soup' />
-          <ProductEntry item='Taco Seasoning' />
-          <ProductEntry item='Tortilla Chips' />
-          <ProductEntry item='Pringles' />
-          <ProductEntry item='Marshmellows' />
-          <ProductEntry item='Pop Tarts' />
-          <ProductEntry item='String Cheese' />
-=======
           <div id='shoppingLeft'>
+            <h3>Store Inventory:</h3>
             <ProductEntry item='Rice' />
             <ProductEntry item='Chicken' />
             <ProductEntry item='Tomato' />
@@ -75,21 +57,16 @@ export default function ShoppingList() {
             <ProductEntry item='Marshmellows' />
             <ProductEntry item='Pop Tarts' />
             <ProductEntry item='String Cheese' />
->>>>>>> dev
 
 
           </div>
           <div id='shoppingRight'>
+            <h3>Grocery List</h3>
 
 
           </div>
         </div>
-<<<<<<< HEAD
-        <div id='shoppingRight'>
-          <h3>Grocery List</h3>
-=======
-        <button>Submit</button>
->>>>>>> dev
+
 
 
       </div >
