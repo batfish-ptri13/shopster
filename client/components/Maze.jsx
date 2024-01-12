62fac62eaab575ed5316be5fae448f818c0bbff2
rmarkowi1990 <rick@fastcasual.co>
2024-01-11 20:26:57 -0800

    now with maze rendering

--- client/components/Maze.jsx
+++ client/components/Maze.jsx
@@ -5,9 +5,11 @@
 
 import React, { useRef, useEffect } from 'react';
 import Nav from './Nav.jsx';
+import Square from './Square.jsx'
+import Row from './Row.jsx'
 
 const Maze = () => {
-  const canvasRef = useRef(null);
+  // const canvasRef = useRef(null);
   // const layout = [
   //     [1, 0, 0, 0, 0],
   //     [1, 0, 1, 1, 0],
@@ -17,42 +19,74 @@ const Maze = () => {
   const layout = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0],
     [0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [0, 1, 1, 1, 1, 2, 1, 1, 1, 1, 0, 1],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
   ];
 
-  useEffect(() => {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext('2d');
-      const tileSize = 50; // Adjust size as needed
+  // useEffect(() => {
+  //   const canvas = canvasRef.current;
+  //   const ctx = canvas.getContext('2d');
+  //   const tileSize = 50; // Adjust size as needed
 
-      for (let row = 0; row < layout.length; row++) {
-          for (let col = 0; col < layout[row].length; col++) {
-              if (layout[row][col] === 1) {
-                  ctx.fillStyle = 'brown'; // Shelf color
-                  ctx.fillRect(col * tileSize, row * tileSize, tileSize, tileSize);
-              } else {
-                  ctx.fillStyle = 'lightgrey'; // Aisle color
-                  ctx.fillRect(col * tileSize, row * tileSize, tileSize, tileSize);
-              }
-          }
-      }
+  //   for (let row = 0; row < layout.length; row++) {
+  //     for (let col = 0; col < layout[row].length; col++) {
+  //       if (layout[row][col] === 1) {
+  //         ctx.fillStyle = 'brown'; // Shelf color
+  //         ctx.fillRect(col * tileSize, row * tileSize, tileSize, tileSize);
+  //       } else {
+  //         ctx.fillStyle = 'lightgrey'; // Aisle color
+  //         ctx.fillRect(col * tileSize, row * tileSize, tileSize, tileSize);
+  //       }    
+  //     }
+  //   }
 
-  }, []);
+  // }, []);
+
+
+
+  const render = layout.map((row, index) => {
+    return (
+      <Row row={row} />
+
+    )
+  })
+
+  // const render = layout.map((row, rowIdx) => {
+  //   return (
+  //     <Row key={rowIdx}>
+  //       {row.map((cell, cellIdx) => {
+  //         return (
+  //           <Square key={cellIdx}/>
+  //         )
+  //       })}
+  //     </Row>
+  //   )
+  // })
 
   return (
     <div>
-      <Nav/>
-      <h1>Maze component</h1>
-      <canvas ref={canvasRef} width="1000" height="550" id="storeLayoutCanvas"/>
+      <Nav />
+      <div className='mazeContainer'>
+
+        <div className='mazeOutline'>
+          {render}
+
+
+        </div>
+
+
+
+        {/* <h1>Maze component</h1>
+      <canvas ref={canvasRef} width="1000" height="550" id="storeLayoutCanvas"/> */}
+      </div>
     </div>
-  ) 
+  )
 };
 
 export default Maze;
---
+++ client/components/Square.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function (props) {
+
+
+    let id = ''
+    if (props.number === 0) {
+        id = 'aisle'
+    } else if (props.number === 1) {
+        id = 'shelf'
+    } else if (props.number === 2) {
+        id = 'product'
+    }
+
+
+
+
+
+    return (
+        <div className='box' id={id}>
+            {/* {props.number} */}
+        </div>
+    )
+
+
+}
