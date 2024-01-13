30b500dffe86b7f79d94f5076409da847ad90f08
[Emi Knox-Hershey] <emiluzartist@gmail.com>
2024-01-13 15:32:14 -0500

    debugging submitList controller

--- server/controllers/listController.js
+++ server/controllers/listController.js
@@ -24,44 +24,61 @@ listController.getAll = (req, res, next) => {
     // 2nd login- user gets previous shopping, makes changes, hit submit --> conditional DELETE FROM shoppingList WHERE user_id === currUserId, INSERT all new values
 listController.submitList = (req, res, next) => {
   const {user_id, productsArr} = req.body;
-  // console.log('entered listController.submitList, req.body: ', req.body);
+  console.log('entered listController.submitList, req.body: ', req.body);
 
   // loop to call query and insert into for each product in array
-  productsArr.forEach( prod_id => {
+  let sl_prod_location_x = 0;
+  let sl_prod_location_y = 0;
+
+  productsArr.forEach( async function (prod_id) {
     // query to find corresponding location data? Unnesecary if we send prod_location_x and y back on req object from frontend, maybe in an array of objects [{prod_id: __, prod_location_x:__, prod_location_y:__}]
     console.log('entered forEach loop listController.submitList, prod_id: ', prod_id);
     
-    db.query(`SELECT * FROM products WHERE prod_id === ${prod_id}`)
-    .then(data => {
-      const sl_prod_location_x = data.rows.prod_location_x;
-      const sl_prod_location_y = data.rows.prod_location_y;
-    })
-    .catch(err => {
-      return console.log('Error found in location query, productsArr.forEach, listController.submitList: ', err)
-    })
+    // await db.query(`SELECT prod_location_x, prod_location_y FROM products WHERE prod_id = ${prod_id}`)
+    // .then(data => {
+    //   console.log('entered location query in forEach loop listController.submitList: ', data.rows);
+    //   sl_prod_location_x = data.rows[0].prod_location_x;
+    //   sl_prod_location_y = data.rows[0].prod_location_y;
+    //   console.log('location query assign sl_prod_location_x: ', sl_prod_location_x);
+    // })
+    // .catch(err => {
+    //   return console.log('Error found in location query, productsArr.forEach, listController.submitList: ', err)
+    // })
+
+    const locatQuery = `SELECT prod_location_x, prod_location_y FROM products WHERE prod_id = ${prod_id}`;
+
+    const result = await db.query(locatQuery)
+    console.log('received result in forEach loop, fileController.submitList: ', result.rows);
+    sl_prod_location_x = result.rows[0].prod_location_x;
+    sl_prod_location_y = result.rows[0].prod_location_y;
+
+    console.log('sl_prod_location_x: ', sl_prod_location_x);
+    console.log('sl_prod_location_y: ', sl_prod_location_y);
+
     //query string for Insert query
-    const query = `
-      INSERT INTO shopping_list (
-        sl_quantity,
-        sl_in_cart,
-        sl_user_id,
-        sl_prod_id,
-        sl_prod_location_x,
-        sl_prod_location_y
-      ) VALUES (
-        1,
-        false,
-        ${user_id},
-        ${prod_id},
-        ${sl_prod_location_x},
-        ${sl_prod_location_y}
-      )
-    `;
-    // Insert query to add record to shopping_list table
-    db.query(query)
-      .catch(err => {
-        return console.log('Error found in Insert query, productsArr.forEach, listController.submitList: ', err)
-      })
+    console.log('After location query, before Insert query string');
+  //   const query = `
+  //     INSERT INTO shopping_list (
+  //       sl_quantity,
+  //       sl_in_cart,
+  //       sl_user_id,
+  //       sl_prod_id,
+  //       sl_prod_location_x,
+  //       sl_prod_location_y
+  //     ) VALUES (
+  //       1,
+  //       false,
+  //       ${user_id},
+  //       ${prod_id},
+  //       ${sl_prod_location_x},
+  //       ${sl_prod_location_y}
+  //     )
+  //   `;
+  //   // Insert query to add record to shopping_list table
+  //   db.query(query)
+  //     .catch(err => {
+  //       return console.log('Error found in Insert query, productsArr.forEach, listController.submitList: ', err)
+  //     })
   });
   return next();
 };
