373958c29465b6c86cd8a26c8b3c8e3923eae0e4
Rob <rob@laserleadmarketing.com>
2024-01-13 20:51:14 -0500

    Added Signup and Login JSX

--- client/components/auth/Login.jsx
+++ client/components/auth/Login.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import * as stylex from '@stylexjs/stylex';
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -9,56 +10,88 @@ export default function Login(){
  
   return (
   
-  <>
-<div {...stylex.props(styles.scafold)}>
-  <div {...stylex.props(styles.loginBox)} >
-  <div>
-    Login
-  </div>
+    <>
+      <div {...stylex.props(styles.scafold)}>
+        <div {...stylex.props(styles.loginBox)} >
+        
  
-  <div {...stylex.props(styles.inputWrapper)}>
-  <div>Name</div>
-    <input {...stylex.props(styles.input)}/>
-</div>
+          <div>How Would You Like To Login</div>
+          <div {...stylex.props(styles.buttonWrapper)}>
+         
+           
+            <button {...stylex.props(styles.button)}> User Name and Password</button>
+            <button {...stylex.props(styles.button)}> Magic Link</button>
+            <button {...stylex.props(styles.button)}> Text Code</button>
+          </div>
+         
   
+          <div  {...stylex.props(styles.signUp)}>
+            <div>Need an account? Signup</div>
+            <NavLink to={'/signup'} {...stylex.props(styles.link)}>Here</NavLink> 
+          </div>
+        </div>
+       
+      </div>
 
-
-  </div>
-  </div>
-
-  </>
-  )
+    </>
+  );
 }
 
 const styles = stylex.create({
-    scafold:{
-        width:'100%',
-        height: '100vh',
-        display:'flex',
-        alignItems:'center',
-        justifyContent:'center',
-    },
-    loginBox: {
-        width:'400px',
-        display:'flex',
-        flexDirection:'column',
-        padding:'20px',
-        alignItems:'center',
-        height: '400px',
-        backgroundColor:'#312f32',
-      fontSize: 18,
-      lineHeight: 1.5,
-      color: {
-        'default':'#e8e7d5'
-        }
-    },
-    inputWrapper:{
-        marginTop:'20px',
-     width: '100%'
-    },
-    input:{
-        height:'20px',
-        width: '80%'
+  scafold:{
+    width:'100%',
+    height: '100vh',
+    display:'flex',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  loginBox: {
+    position:'relative',
+    width:'400px',
+    display:'flex',
+    flexDirection:'column',
+    padding:'20px',
+    alignItems:'center',
+    height: '400px',
+    backgroundColor:'#312f32',
+    fontSize: 18,
+    lineHeight: 1.5,
+    color: {
+      'default':'#e8e7d5'
     }
+  },
+  buttonWrapper:{
+    display:'flex',
+    alignItems:'center',
+    flexDirection:'column',
+    marginTop:'20px',
+    width: '100%',
+   
+  },
+  button:{
+    height:'40px',
+    width: '50%',
+    marginTop:'20px',
+    borderRadius: '4px',
+    backgroundColor:{
+      default: '#e8e7d5',
+      ':hover': "#d5e7e8"
+    }
+    
+  },
+  signUp:{
+    position:'absolute',
+    display:'flex',
+    justifyItems:'center',
+    bottom:'0',
+    right:'0'
+  },
+  link:{
+    color: {
+      'default':'#e8e7d5',
+      ":hover": "#d5e7e8"
+    },
+    marginLeft:'4px'
+  }
   
-  });
+});
--- client/components/auth/SignUp.jsx
+++ client/components/auth/SignUp.jsx
@@ -1,7 +1,109 @@
+
+
 import React from 'react';
+import * as stylex from '@stylexjs/stylex';
 
-export default function Signup(){
+
+
+export default function SignUp(){
  
-  return <></>;
+  return (
+  
+    <>
+      <div {...stylex.props(styles.scafold)}>
+        <div {...stylex.props(styles.loginBox)} >
+        
+ 
+          <div>Create New Account</div>
+          <div {...stylex.props(styles.buttonWrapper)}>
+         
+            <div>First Name</div>
+            <input {...stylex.props(styles.input)}/> 
 
+            <div>Last Name</div>
+            <input {...stylex.props(styles.input)}/>
+
+            <div>Email</div>
+            <input {...stylex.props(styles.input)}/>
+
+            <div>Phone</div>
+            <input {...stylex.props(styles.input)}/>
+
+          
+          
+          </div>
+         
+        
+
+
+        </div>
+
+        <button {...stylex.props(styles.submitButton)}> Submit</button>
+      </div>
+
+    </>
+  );
 }
+
+const styles = stylex.create({
+  scafold:{
+    width:'100%',
+    height: '100vh',
+    display:'flex',
+    flexDirection:'column',
+    alignItems:'center',
+    justifyContent:'center',
+  },
+  loginBox: {
+    width:'400px',
+    display:'flex',
+    flexDirection:'column',
+    padding:'40px',
+    alignItems:'center',
+    height: 'auto',
+    backgroundColor:'#312f32',
+    fontSize: 18,
+    lineHeight: 1.5,
+    color: {
+      'default':'#e8e7d5'
+    }
+  },
+  buttonWrapper:{
+    display:'flex',
+    alignItems:'center',
+    flexDirection:'column',
+    marginTop:'20px',
+    width: '100%',
+   
+  },
+
+  input:{
+    display:'flex',
+    alignItems:'center',
+    flexDirection:'column',
+    marginTop:'20px',
+    width: '50%',
+    height:'40px',
+    marginBottom:'10px',
+    backgroundColor:{
+      default: '#e8e7d5',
+      ':hover': "#d5e7e8"
+    }
+  },
+  submitButton:{
+    height:'50px',
+    width: '200px',
+    marginTop:'10px',
+    borderRadius: '4px',
+    backgroundColor:{
+      default: '#312f32',
+      ':hover': "#d5e7e8"
+    },
+    color:{
+      default:"#d5e7e8",
+      ':hover':'#312f32',
+    }
+    
+  }
+  
+});
