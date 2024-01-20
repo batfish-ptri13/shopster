80d97f93eff94b05ce5c395ee64b09b24e814fb2
rmarkowi1990 <rick@fastcasual.co>
2024-01-20 12:03:47 -0800

    errors in auth

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,22 +4,43 @@
 */
 
 import React, { useRef, useEffect } from 'react';
-import Nav from './Nav.jsx';
-import { NavLink} from 'react-router-dom';
+import NavNotLogged from './NavNotLogged.jsx';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 
 
 
 
-export default function Home () {
+// import lobster from '/shopster_lobster_logo_01.png'
 
 
+
+
+
+export default function Home() {
+
+  const navigate = useNavigate()
+
+
+  // <NavLink to={'/login'} > Login</NavLink>
+
+  function onClick() {
+    navigate('/login')
+
+
+  }
 
   return (
     <div>
-      <Nav />
-      Welcome to Shopster
-      <NavLink to={'/login'} > Login</NavLink>
+      <NavNotLogged />
+      <div className='homeContainer'>
+
+        {/* <h1> Welcome to Shopster</h1> */}
+        <img src='/shopster_lobster_logo_01.png'></img>
+        <button onClick={onClick}>Login!</button>
+
+      </div>
+
     </div>
   );
 }
